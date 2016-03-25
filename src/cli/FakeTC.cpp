9377703c165c75b2bd441faf6f44654415f54b64
jackpoz <giacomopoz@gmail.com>
2016-03-25 18:03:28 +0100

    Fix build after last merge

--- src/cli/FakeTC.cpp
+++ src/cli/FakeTC.cpp
@@ -13,6 +13,12 @@ SpellMgr::SpellMgr()
 SpellMgr::~SpellMgr()
 {}
 
+SpellMgr* SpellMgr::instance()
+{
+    static SpellMgr instance;
+    return &instance;
+}
+
 void SpellMgr::SetSpellDifficultyId(uint32 spellId, uint32 id)
 {
 }
