b236319c0e786d921ee06b5cb4f02d31d158d471
sveN295 <svengrt@gmail.com>
2022-05-03 23:13:13 +0200

    Passive AntiCheat v1

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -78,6 +78,7 @@
 #include "World.h"
 #include "WorldPacket.h"
 #include "WorldSession.h"
+#include "AntiCheatMgr.h"
 #include <cmath>
 
 float baseMoveSpeed[MAX_MOVE_TYPE] =
@@ -8649,6 +8650,10 @@ void Unit::UpdateSpeed(UnitMoveType mtype)
     }
 
     SetSpeedRate(mtype, speed);
+
+    // If the Speed Changes we dont want to have a false positive on our speed hack
+    if (Player* player = this->ToPlayer())
+        player->GetAntiCheat()->SetAllowedMovement(true);
 }
 
 float Unit::GetSpeed(UnitMoveType mtype) const
@@ -12700,6 +12705,10 @@ void Unit::_ExitVehicle(Position const* exitPosition)
     /// from the target in the aforementioned function and we don't need to do anything else at this point.
     if (!m_vehicle)
         return;
+
+    // We need to call this so two player mounts dont trigger hack detection
+    if (Player* player = this->ToPlayer())
+        player->GetAntiCheat()->SetAllowedMovement(true);
 
     // This should be done before dismiss, because there may be some aura removal
     VehicleSeatAddon const* seatAddon = m_vehicle->GetSeatAddonForSeatOfPassenger(this);
